aca5cff96e9b9d8b725b0c349cd6c1772d353e7a
Dave Murray <138507286+Dave-Murray98@users.noreply.github.com>
2026-03-01 19:08:32 +0000

    Added Digger asset

---
+++ Assets/Digger/Modules/Runtime/Sources/Editor/PreprocessBuild.cs
@@ -0,0 +1,54 @@
+using System.IO;
+using Digger.Modules.Core.Sources;
+using Digger.Modules.Core.Sources.Jobs;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Digger.Modules.Runtime.Sources.Editor
+{
+    public class PreprocessBuild : BuildPlayerProcessor
+    {
+        public override void PrepareForBuild(BuildPlayerContext buildPlayerContext)
+        {
+            if (Application.isPlaying)
+                return;
+
+            Debug.Log("PreprocessBuild.PrepareForBuild");
+            NativeCollectionsPool.Instance.Dispose();
+
+            var streamingAssetsBasePath = Path.Combine(Application.streamingAssetsPath, "DiggerData");
+            if (Directory.Exists(streamingAssetsBasePath))
+                Directory.Delete(streamingAssetsBasePath, true);
+
+            var scenes = buildPlayerContext.BuildPlayerOptions.scenes;
+            foreach (var scene in scenes) {
+                ProcessScene(SceneManager.GetSceneByPath(scene));
+            }
+        }
+
+        private void ProcessScene(Scene scene)
+        {
+            Debug.Log($"PreprocessBuild.ProcessScene: {scene.name}");
+            var rootObjects = scene.GetRootGameObjects();
+
+            var includeVoxelData = false;
+            foreach (var rootObject in rootObjects) {
+                var diggerRuntime = rootObject.GetComponentInChildren<DiggerMasterRuntime>();
+                if (diggerRuntime) {
+                    includeVoxelData = true;
+                    Debug.Log($"DiggerMasterRuntime has been detected in scene '{scene.name}'. Voxel data will be included in build.");
+                    break;
+                }
+            }
+
+            foreach (var rootObject in rootObjects) {
+                var diggers = rootObject.GetComponentsInChildren<DiggerSystem>();
+                foreach (var digger in diggers) {
+                    digger.OnPreprocessBuild(includeVoxelData);
+                }
+            }
+        }
+    }
+}

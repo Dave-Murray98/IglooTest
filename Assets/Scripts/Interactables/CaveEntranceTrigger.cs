ea007ffac501273d73dea4804694ed6cc7e8c04a
Dave Murray <138507286+Dave-Murray98@users.noreply.github.com>
2026-03-06 19:49:35 +0000

    Added rock debris falling sfx

--- Assets/Scripts/Interactables/CaveEntranceTrigger.cs
+++ Assets/Scripts/Interactables/CaveEntranceTrigger.cs
@@ -7,12 +7,22 @@ public class CaveEntranceTrigger : EventTrigger
 
     [SerializeField] private GameObject npcsParent;
 
+    [SerializeField] private AudioClip rockDebrisSound;
+
+    [SerializeField] private Transform rockDebrisTransform;
+
     protected override void TriggerEvent()
     {
         base.TriggerEvent();
 
         rockDebrisAnimator.SetTrigger(rockDebrisAnimationName);
         npcsParent.SetActive(false);
+    }
+
+    public void PlayRockDebrisSound()
+    {
+        if (rockDebrisSound != null)
+            AudioManager.Instance.PlaySound(rockDebrisSound, rockDebrisTransform.position, AudioCategory.Ambience, layer: AudioLayer.Exterior);
     }
 
 

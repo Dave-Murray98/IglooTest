4fd59c76fac1d96db14b52b39f5f1e6133b80af2
Dave Murray <138507286+Dave-Murray98@users.noreply.github.com>
2026-08-21 11:16:39 +0100

    Created steam page assets folder with objects

--- Assets/Scripts/Camera System/ScreenshotCapture.cs
+++ Assets/Scripts/Camera System/ScreenshotCapture.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 
 public class ScreenshotCapture : MonoBehaviour
 {
+    [SerializeField] private string path = "Assets/Resources/Art/ScreenCaptures/";
+    [Range(1, 5)]
+    [SerializeField] private int size = 2;
+
+    [SerializeField] private KeyCode captureKey = KeyCode.P;
+
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(captureKey))
         {
             // The "4" multiplies your current resolution by 4x
-            ScreenCapture.CaptureScreenshot("Assets/Resources/Art/ScreenCaptures/PosterShot" + System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png", 4);
+            ScreenCapture.CaptureScreenshot(path + "Screenshot" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".png", size);
             Debug.Log("Screenshot taken!");
         }
     }
